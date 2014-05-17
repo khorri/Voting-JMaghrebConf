cc39ec554cd8f442b2619bfe108b1df19aa63c65
Yousri <b.yousri@gmail.com>
2014-05-17 18:41:54 +0100

    Version trés minim Développé par Abdessamad

---
+++ RaspberryPi/src/pi/Pi.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pi;
+
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalInput;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinPullResistance;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
+import com.pi4j.io.gpio.event.GpioPinListener;
+import com.pi4j.io.gpio.event.GpioPinListenerDigital;
+import com.pi4j.io.gpio.trigger.GpioCallbackTrigger;
+import com.pi4j.io.gpio.trigger.GpioSetStateTrigger;
+import com.pi4j.io.gpio.trigger.GpioSyncStateTrigger;
+import com.pi4j.io.gpio.trigger.GpioTrigger;
+import java.util.concurrent.Callable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Naciri
+ */
+public class Pi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        try {
+            GpioPinDigitalInput myButton;
+            GpioPinDigitalOutput myLed;
+            
+            GpioController myController = GpioFactory.getInstance();
+            GpioController ledController = GpioFactory.getInstance();
+            
+            myButton=myController.provisionDigitalInputPin(RaspiPin.GPIO_02,PinPullResistance.PULL_DOWN);
+            myLed =ledController.provisionDigitalOutputPin(RaspiPin.GPIO_07,"LED #1",PinState.LOW);
+            
+            myButton.addTrigger(new GpioSetStateTrigger(PinState.HIGH, myLed, PinState.HIGH));
+            myButton.addTrigger(new GpioSetStateTrigger(PinState.LOW, myLed, PinState.LOW));
+            
+            
+            
+            
+           /* myButton.addListener(new GpioPinListenerDigital() {
+                
+                @Override
+                public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
+                    
+                    System.out.println("Hello");
+                    
+                }
+            });
+            
+            System.out.println("Listener Ready");*/
+            
+            myButton.addTrigger(new GpioCallbackTrigger(new Callable<Void>() {
+            public Void call() throws Exception {
+                System.out.println(" --> I LIKED IT ");
+                return null;
+            }
+        }));
+
+            
+            for(;;)
+                Thread.sleep(500);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Pi.class.getName()).log(Level.SEVERE, null, ex);
+        }
+   
+        
+    }
+    
+}
